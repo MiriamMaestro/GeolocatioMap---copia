b9a057d6fa12b814da60415e49bce7f0be8e32eb
Miriam Maestro <yosoymairim@gmail.com>
2021-02-08 17:25:39 +0000

    nieva

--- js/map.js
+++ js/map.js
@@ -30,118 +30,50 @@ map.on('locationfound', onLocationFound);
 map.on('locationerror', onLocationError);
 map.locate({setView: false});
 
-/*
-L.tileLayer.provider('MapBox', {
-    id: 'mapbox/streets-v11',
-    accessToken: 'pk.eyJ1IjoieW9zb3ltYWlyaW0iLCJhIjoiY2trOGoyb2oxMDk2eDJ2czlzNm0wbnVmaSJ9.HvIfpKYqyZHeCNZPIpPSaw'
-  }).addTo(map);
-
-  function onLocationFound(e) {
-    var radius = e.accuracy / 2;
-
-    L.marker(e.latlng).addTo(map)
-    .bindPopup("You are here")
-        
-    L.circle(e.latlng, radius).addTo(map);
-    
-    var clatitud = e.latlng.lat;
-    currentLat=clatitud;
-    var clongitud = e.latlng.lng;
-    currentLong= clongitud;
-
-}
-var currentLat="";
-var currentLong="";
-
-function onLocationError(e) {
-    alert(e.message);
-}
-
-
-map.on('locationfound', onLocationFound);
-map.on('locationerror', onLocationError);
-
-//map.locate({setView: true, maxZoom: 8});
-map.locate({setView: true, maxZoom: 8});
-*/
-
-var options = {
-    key: '1241387f14a84f768dc7bac0933c0a72',
-    limit: 10,
-    proximity: '51.52255, -0.10249' // favour results near here
-  };
-  var geocoder = L.Control.OpenCageSearch.geocoder(options);
-  //var control = L.Control.openCageSearch(options).addTo(map);
-  var marker;
-
-
-map.on('click', function(e) {
-    var query = e.latlng.lat.toString() + ',' + e.latlng.lng.toString();
-    geocoder.geocode(query, function(results) {
-      var r = results[0];
-      if (r) {
-        if (marker) {
-          marker.setLatLng(r.center).setPopupContent(r.name).openPopup();
-        } else {
-          marker = L.marker(r.center).bindPopup(r.name).addTo(map).openPopup();
-        }
-      }
-    })
-    var latitud = e.latlng.lat;
-    lat=latitud;
-    var longitud = e.latlng.lng;
-    long= longitud;
-});
-var lat="";
-var long="";
-//Weather api
-
 $(document).ready(() => {
-    const $btnRun = $('#weather');
+  const $btnRun = $('#weather');
 
 $btnRun.on('click', () => {
-    $.ajax({
-    url: "php/weather.php",
-    type: 'POST',
-    dataType: 'json',
-    data: {
-      lat: (lat == "") ? latitudecurrent:  lat  ,
-      long: (long == "") ? longitudecurrent:  long 
-     },
-    success: function(result) {
-      console.log(result);
-        if (result.status.name == "ok") {
-          function temperatureConverter(valNum) {
-            valNum = parseFloat(valNum);
-            dec = valNum-273.15;
-            return parseFloat(dec).toFixed(0);
-          }
-          
-          var tiemp = result['data']['main']['temp'];
-          var tempM =result['data']['main']['temp_max'];
-          var tempm =result['data']['main']['temp_min'];
-          var fl=result['data']['main']['feels_like'];
-            $('#temperature').html(temperatureConverter(tiemp)).append(" º");
-            $('#temp-max').html(temperatureConverter(tempM)).append(" º");
-            $('#humidity').html(result['data']['main']['humidity']).append(" %");
-            $('#pressure').html(result['data']['main']['pressure']);
-            $('#feels-like').html(temperatureConverter(fl)).append(" º");
-            $('#temp-min').html(temperatureConverter(tempm)).append(" º");
-            $('#description').html(result['data']['weather'][0]['main']);
-            $('#city').html(result['data']['name']);
-
+  $.ajax({
+  url: "php/weathercountry.php",
+  type: 'POST',
+  dataType: 'json',
+  data: {
+    iso: $( "#inlineFormCustomSelect" ).val(),
+  },
+  success: function(result) {
+    console.log(result);
+      if (result.status.name == "ok") {
+        function temperatureConverter(valNum) {
+          valNum = parseFloat(valNum);
+          dec = valNum-273.15;
+          return parseFloat(dec).toFixed(0);
         }
-    },
-    error: function(jqXHR, textStatus, errorThrown) {
-        console.log("it´s not working");
-    }
+        
+        var tiemp = result['data']['main']['temp'];
+        var tempM =result['data']['main']['temp_max'];
+        var tempm =result['data']['main']['temp_min'];
+        var fl=result['data']['main']['feels_like'];
+          $('#temperature').html(temperatureConverter(tiemp)).append(" º");
+          $('#temp-max').html(temperatureConverter(tempM)).append(" º");
+          $('#humidity').html(result['data']['main']['humidity']).append(" %");
+          $('#pressure').html(result['data']['main']['pressure']);
+          $('#feels-like').html(temperatureConverter(fl)).append(" º");
+          $('#temp-min').html(temperatureConverter(tempm)).append(" º");
+          $('#description').html(result['data']['weather'][0]['main']);
+          $('#city').html(result['data']['name']);
+
+      }
+  },
+  error: function(jqXHR, textStatus, errorThrown) {
+      console.log("it´s not working");
+  }
 }); 
 }).on('click', () => {
-  $('.tabla-weather').toggle();
+$('.tabla-weather').toggle();
 });
 
 });
-
 //Information
 
 $(document).ready(() => {
@@ -419,7 +351,7 @@ $(window).on("load", ()=>{
               
                 for (i=0; i<result['data']['features']['features'].length ; i++) {
                   var tag = document.createElement('option');
-                  tag.value= result['data']['features']['features'][i]["properties"]["iso_a2"] ;
+                  tag.value= result['data']['features']['features'][i]["properties"]["name"] ;
                   tag.text = result['data']['features']['features'][i]["properties"]["name"] ;
                   var element = document.getElementById('inlineFormCustomSelect');
                   element.appendChild(tag);
